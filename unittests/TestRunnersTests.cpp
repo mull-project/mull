b56130eee91229e08d998235521b37fbfef09ca5
AlexDenisov <1101.debian@gmail.com>
2016-04-30 09:40:51 +0200

    Add test using libc

--- unittests/TestRunnersTests.cpp
+++ unittests/TestRunnersTests.cpp
@@ -21,7 +21,13 @@ static LLVMContext GlobalCtx;
 
 static std::unique_ptr<Module> parseIR(const char *IR) {
   SMDiagnostic Err;
-  return parseAssemblyString(IR, Err, GlobalCtx);
+  auto M = parseAssemblyString(IR, Err, GlobalCtx);
+
+  /// FIXME: is there another way to check for errors?
+  if (Err.getMessage().str().length() != 0) {
+    Err.print("test", dbgs());
+  }
+  return M;
 }
 
 TEST(SimpleTestRunner, runTest) {
@@ -51,6 +57,94 @@ TEST(SimpleTestRunner, runTest) {
                                    "  %add = add nsw i32 %0, %1\n"
                                    "  ret i32 %add\n"
                                    "}");
+
+  Context Ctx;
+  Ctx.addModule(ModuleWithTests.get());
+  Ctx.addModule(ModuleWithTestees.get());
+
+  SimpleTestFinder Finder(Ctx);
+  ArrayRef<Function *> Tests = Finder.findTests();
+
+  ASSERT_NE(0U, Tests.size());
+
+  Function *Test = *(Tests.begin());
+
+  std::vector<Module *> Modules;
+  Modules.push_back(ModuleWithTests.get());
+
+  SimpleTestRunner Runner(Modules);
+
+  /// Here we run test with original testee function
+  ASSERT_EQ(Passed, Runner.runTest(Test, ModuleWithTestees.get()));
+
+  /// afterwards we apply single mutation and run test again
+  /// expecting it to fail
+
+  ArrayRef<Function *> Testees = Finder.findTestees(*Test);
+  ASSERT_NE(0U, Testees.size());
+  Function *Testee = *(Testees.begin());
+
+  AddMutationOperator MutOp;
+  ArrayRef<MutationOperator *> MutOps(&MutOp);
+
+  std::vector<std::unique_ptr<MutationPoint>> MutationPoints = Finder.findMutationPoints(MutOps, *Testee);
+
+  MutationPoint *MP = (*(MutationPoints.begin())).get();
+  MutationEngine Engine;
+  Engine.applyMutation(*MP);
+
+  ASSERT_EQ(Failed, Runner.runTest(Test, ModuleWithTestees.get()));
+}
+
+TEST(SimpleTestRunner, runTestUsingLibC) {
+  auto ModuleWithTests = parseIR("@.str = private unnamed_addr constant [6 x i8] c\"ababa\\00\", align 1 \n"
+                                 "define i32 @test_count_letters() { \n"
+                                 "entry: \n"
+                                 "  %call = call i32 @count_letters(i8* getelementptr inbounds ([6 x i8], [6 x i8]* @.str, i32 0, i32 0), i8 signext 97) \n"
+                                 "  %cmp = icmp eq i32 %call, 3 \n"
+                                 "  %conv = zext i1 %cmp to i32 \n"
+                                 "  ret i32 %conv \n"
+                                 "} \n"
+                                 ""
+                                 "declare i32 @count_letters(i8*, i8 signext) \n");
+
+  auto ModuleWithTestees = parseIR("define i32 @count_letters(i8* %s, i8 signext %c) {\n"
+                                   "entry:\n"
+                                   "  %s.addr = alloca i8*, align 8\n"
+                                   "  %c.addr = alloca i8, align 1\n"
+                                   "  %count = alloca i32, align 4\n"
+                                   "  %found = alloca i8*, align 8\n"
+                                   "  store i8* %s, i8** %s.addr, align 8\n"
+                                   "  store i8 %c, i8* %c.addr, align 1\n"
+                                   "  store i32 0, i32* %count, align 4\n"
+                                   "  %0 = load i8*, i8** %s.addr, align 8\n"
+                                   "  store i8* %0, i8** %found, align 8\n"
+                                   "  br label %while.cond\n"
+                                   ""
+                                   "while.cond: ; preds = %while.body, %entry\n"
+                                   "  %1 = load i8*, i8** %found, align 8\n"
+                                   "  %2 = load i8, i8* %c.addr, align 1\n"
+                                   "  %conv = sext i8 %2 to i32\n"
+                                   "  %call = call i8* @strchr(i8* %1, i32 %conv)\n"
+                                   "  store i8* %call, i8** %found, align 8\n"
+                                   "  %tobool = icmp ne i8* %call, null\n"
+                                   "  br i1 %tobool, label %while.body, label %while.end\n"
+                                   ""
+                                   "while.body: ; preds = %while.cond\n"
+                                   "  %3 = load i32, i32* %count, align 4\n"
+                                   "  %add = add nsw i32 %3, 1\n"
+                                   "  store i32 %add, i32* %count, align 4\n"
+                                   "  %4 = load i8*, i8** %found, align 8\n"
+                                   "  %add.ptr = getelementptr inbounds i8, i8* %4, i64 1\n"
+                                   "  store i8* %add.ptr, i8** %found, align 8\n"
+                                   "  br label %while.cond\n"
+                                   ""
+                                   "while.end:  ; preds = %while.cond\n"
+                                   "  %5 = load i32, i32* %count, align 4\n"
+                                   "  ret i32 %5\n"
+                                   "}\n"
+                                   ""
+                                   "declare i8* @strchr(i8*, i32)");
 
   Context Ctx;
   Ctx.addModule(ModuleWithTests.get());
