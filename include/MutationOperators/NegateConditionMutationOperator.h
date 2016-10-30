e3ed10e4bf552e1bd6065130e4364f2e04f21d2c
AlexDenisov <1101.debian@gmail.com>
2016-10-30 12:12:34 +0100

    Add Negate Condition operator into driver

--- include/MutationOperators/NegateConditionMutationOperator.h
+++ include/MutationOperators/NegateConditionMutationOperator.h
@@ -17,6 +17,13 @@ namespace Mutang {
     static llvm::CmpInst::Predicate negatedCmpInstPredicate(llvm::CmpInst::Predicate predicate);
     std::vector<MutationPoint *> getMutationPoints(llvm::Function *function) override;
 
+    std::string uniqueID() override {
+      return "negate_mutation_operator";
+    }
+    std::string uniqueID() const override {
+      return "negate_mutation_operator";
+    }
+
     bool canBeApplied(llvm::Value &V) override;
     llvm::Value *applyMutation(llvm::Module *M, MutationPointAddress address, llvm::Value &OriginalValue) override;
     llvm::Value *revertMutation(llvm::Value &Value) override;
