e35583aae28ccfff29697f6e58a83cec5117a64a
Stanislaw Pankevich <s.pankevich@gmail.com>
2017-04-05 22:01:40 +0200

    Initial mull-autotest target.

--- Mull.extensions.cmake
+++ Mull.extensions.cmake
@@ -8,8 +8,8 @@ function(mull_clone_target existing_target new_target)
   get_target_property(existing_target_source_dir ${existing_target} SOURCE_DIR)
   get_target_property(existing_target_sources ${existing_target} SOURCES)
   get_target_property(existing_target_include_directories ${existing_target} INCLUDE_DIRECTORIES)
-
   get_target_property(existing_target_link_libraries ${existing_target} LINK_LIBRARIES)
+  get_target_property(existing_target_install_rpath ${existing_target} INSTALL_RPATH)
 
   set(new_target_sources "")
   foreach(existing_target_source_file ${existing_target_sources})
@@ -27,8 +27,48 @@ function(mull_clone_target existing_target new_target)
   add_library(${new_target} SHARED ${new_target_sources})
 
   set_target_properties(${new_target}
-    PROPERTIES INCLUDE_DIRECTORIES "${existing_target_include_directories}")
+    PROPERTIES
+    INCLUDE_DIRECTORIES "${existing_target_include_directories}"
+    INSTALL_RPATH "${existing_target_install_rpath}"
+  )
 
   target_link_libraries(${new_target} ${existing_target_link_libraries})
 
 endfunction(mull_clone_target)
+
+function(mull_clone_executable_target existing_target new_target)
+  message(STATUS
+    "Mull.mull_clone_executable_target()> cloning target '${existing_target}' -> '${new_target}'...")
+
+  get_target_property(existing_target_source_dir ${existing_target} SOURCE_DIR)
+  get_target_property(existing_target_sources ${existing_target} SOURCES)
+  get_target_property(existing_target_include_directories ${existing_target} INCLUDE_DIRECTORIES)
+  get_target_property(existing_target_link_libraries ${existing_target} LINK_LIBRARIES)
+  get_target_property(existing_target_install_rpath ${existing_target} INSTALL_RPATH)
+
+  set(new_target_sources "")
+  foreach(existing_target_source_file ${existing_target_sources})
+      if (existing_target_source_file MATCHES "^\/")
+        set(new_target_source_file
+          "${existing_target_source_file}")
+      else()
+        set(new_target_source_file
+          "${existing_target_source_dir}/${existing_target_source_file}")
+      endif()
+
+      list(APPEND new_target_sources ${new_target_source_file})
+  endforeach()
+
+  add_executable(${new_target} ${new_target_sources})
+
+  set_target_properties(${new_target}
+    PROPERTIES INCLUDE_DIRECTORIES "${existing_target_include_directories}")
+
+  set_target_properties(${new_target}
+    PROPERTIES
+    INCLUDE_DIRECTORIES "${existing_target_include_directories}"
+    INSTALL_RPATH "${existing_target_install_rpath}"
+  )
+
+  target_link_libraries(${new_target} ${existing_target_link_libraries})
+endfunction(mull_clone_executable_target)
