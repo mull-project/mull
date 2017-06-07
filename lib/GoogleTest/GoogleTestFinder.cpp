8aecab619149f07d89a00855ac33f732fec601f8
AlexDenisov <1101.debian@gmail.com>
2017-06-07 20:59:46 +0200

    Drop dead code (we do not use demangling here)

--- lib/GoogleTest/GoogleTestFinder.cpp
+++ lib/GoogleTest/GoogleTestFinder.cpp
@@ -238,19 +238,6 @@ std::vector<std::unique_ptr<Test>> GoogleTestFinder::findTests(Context &Ctx) {
   return tests;
 }
 
-std::string demangle(const char* name) {
-
-  int status = -4; // some arbitrary value to eliminate the compiler warning
-
-  // enable c++11 by passing the flag -std=c++11 to g++
-  std::unique_ptr<char, void(*)(void*)> res {
-    abi::__cxa_demangle(name, NULL, NULL, &status),
-    std::free
-  };
-
-  return (status==0) ? res.get() : name ;
-}
-
 std::vector<std::unique_ptr<Testee>>
 GoogleTestFinder::findTestees(Test *Test,
                               Context &Ctx,
