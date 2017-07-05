dd2ab2d57c100f177057129c2d600bd52fbcfbbb
AlexDenisov <1101.debian@gmail.com>
2017-07-05 22:19:29 +0200

    Pull in and patch the LogicalDylib

--- include/MullJIT.h
+++ include/MullJIT.h
@@ -16,6 +16,117 @@
 
 using namespace llvm;
 
+namespace mull {
+
+template <typename BaseLayerT,
+typename LogicalModuleResources,
+typename LogicalDylibResources>
+class MullLogicalDylib {
+public:
+  typedef typename BaseLayerT::ModuleSetHandleT BaseLayerModuleSetHandleT;
+private:
+
+  typedef std::vector<BaseLayerModuleSetHandleT> BaseLayerHandleList;
+
+  struct LogicalModule {
+    // Make this move-only to ensure they don't get duplicated across moves of
+    // LogicalDylib or anything like that.
+    LogicalModule(LogicalModule &&RHS)
+    : Resources(std::move(RHS.Resources)),
+    BaseLayerHandles(std::move(RHS.BaseLayerHandles)) {}
+
+    LogicalModule() = default;
+    LogicalModuleResources Resources;
+    BaseLayerHandleList BaseLayerHandles;
+  };
+  typedef std::list<LogicalModule> LogicalModuleList;
+
+public:
+
+  typedef typename BaseLayerHandleList::iterator BaseLayerHandleIterator;
+  typedef typename LogicalModuleList::iterator LogicalModuleHandle;
+
+  MullLogicalDylib(BaseLayerT &BaseLayer) : BaseLayer(BaseLayer) {}
+
+  ~MullLogicalDylib() {
+    for (auto &LM : LogicalModules)
+      for (auto BLH : LM.BaseLayerHandles)
+        BaseLayer.removeModuleSet(BLH);
+  }
+
+  // If possible, remove this and ~LogicalDylib once the work in the dtor is
+  // moved to members (eg: self-unregistering base layer handles).
+  MullLogicalDylib(MullLogicalDylib &&RHS)
+  : BaseLayer(std::move(RHS.BaseLayer)),
+    LogicalModules(std::move(RHS.LogicalModules)),
+    DylibResources(std::move(RHS.DylibResources)) {}
+
+  LogicalModuleHandle createLogicalModule() {
+    LogicalModules.push_back(LogicalModule());
+    return std::prev(LogicalModules.end());
+  }
+
+  void addToLogicalModule(LogicalModuleHandle LMH,
+                          BaseLayerModuleSetHandleT BaseLayerHandle) {
+    LMH->BaseLayerHandles.push_back(BaseLayerHandle);
+  }
+
+  LogicalModuleResources& getLogicalModuleResources(LogicalModuleHandle LMH) {
+    return LMH->Resources;
+  }
+
+  BaseLayerHandleIterator moduleHandlesBegin(LogicalModuleHandle LMH) {
+    return LMH->BaseLayerHandles.begin();
+  }
+
+  BaseLayerHandleIterator moduleHandlesEnd(LogicalModuleHandle LMH) {
+    return LMH->BaseLayerHandles.end();
+  }
+
+  llvm::orc::JITSymbol findSymbolInLogicalModule(LogicalModuleHandle LMH,
+                                      const std::string &Name,
+                                      bool ExportedSymbolsOnly) {
+
+    if (auto StubSym = LMH->Resources.findSymbol(Name, ExportedSymbolsOnly))
+      return StubSym;
+
+    for (auto BLH : LMH->BaseLayerHandles)
+      if (auto Symbol = BaseLayer.findSymbolIn(BLH, Name, ExportedSymbolsOnly))
+        return Symbol;
+    return nullptr;
+  }
+
+  llvm::orc::JITSymbol findSymbolInternally(LogicalModuleHandle LMH,
+                                 const std::string &Name) {
+    if (auto Symbol = findSymbolInLogicalModule(LMH, Name, false))
+      return Symbol;
+
+    for (auto LMI = LogicalModules.begin(), LME = LogicalModules.end();
+         LMI != LME; ++LMI) {
+      if (LMI != LMH)
+        if (auto Symbol = findSymbolInLogicalModule(LMI, Name, false))
+          return Symbol;
+    }
+
+    return nullptr;
+  }
+
+  llvm::orc::JITSymbol findSymbol(const std::string &Name, bool ExportedSymbolsOnly) {
+    for (auto LMI = LogicalModules.begin(), LME = LogicalModules.end();
+         LMI != LME; ++LMI)
+      if (auto Sym = findSymbolInLogicalModule(LMI, Name, ExportedSymbolsOnly))
+        return Sym;
+    return nullptr;
+  }
+  
+  LogicalDylibResources& getDylibResources() { return DylibResources; }
+  
+protected:
+  BaseLayerT BaseLayer;
+  LogicalModuleList LogicalModules;
+  LogicalDylibResources DylibResources;
+};
+
 /// @brief Custom Compile On Demand layer
 template <typename BaseLayerT,
 typename CompileCallbackMgrT = llvm::orc::JITCompileCallbackManager,
@@ -131,7 +242,7 @@ private:
     ModuleAdderFtor ModuleAdder;
   };
 
-  typedef llvm::orc::LogicalDylib<BaseLayerT, LogicalModuleResources,
+  typedef MullLogicalDylib<BaseLayerT, LogicalModuleResources,
   LogicalDylibResources> CODLogicalDylib;
 
   typedef typename CODLogicalDylib::LogicalModuleHandle LogicalModuleHandle;
@@ -398,8 +509,6 @@ private:
     auto &LMResources = LD.getLogicalModuleResources(LMH);
     Module &SrcM = LMResources.SourceModule->getResource();
 
-    SrcM.dump();
-
     // Create the module.
     std::string NewName = SrcM.getName();
     NewName += ".";
@@ -472,7 +581,7 @@ private:
   }
 
   void emitCallTreeCallbacks(Module &module) {
-    module.dump();
+    printf("%s\n", module.getModuleIdentifier().c_str());
   }
 
   BaseLayerT &BaseLayer;
@@ -507,3 +616,4 @@ public:
 
   CODLayer &jit() { return compileOnDemandLayer; }
 };
+}
