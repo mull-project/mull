e717607cb1febf5dff65cb231ca6953e9665375f
AlexDenisov <1101.debian@gmail.com>
2017-10-03 21:52:58 +0200

    Remove MullLogicalDylib class

--- include/MullJIT.h
+++ include/MullJIT.h
@@ -47,116 +47,11 @@ void mull_moveFunctionBody(Function &OrigF, ValueToValueMapTy &VMap,
 typedef llvm::orc::ObjectLinkingLayer<> LinkingLayer;
 typedef llvm::orc::IRCompileLayer<LinkingLayer> CompileLayer;
 
-template <typename LogicalModuleResources,
-typename LogicalDylibResources>
-class MullLogicalDylib {
-public:
-  typedef typename CompileLayer::ModuleSetHandleT BaseLayerModuleSetHandleT;
-private:
-
-  typedef std::vector<BaseLayerModuleSetHandleT> BaseLayerHandleList;
-
-  struct LogicalModule {
-    // Make this move-only to ensure they don't get duplicated across moves of
-    // LogicalDylib or anything like that.
-    LogicalModule(LogicalModule &&RHS)
-    : Resources(std::move(RHS.Resources)),
-    BaseLayerHandles(std::move(RHS.BaseLayerHandles)) {}
-
-    LogicalModule() = default;
-    LogicalModuleResources Resources;
-    BaseLayerHandleList BaseLayerHandles;
-  };
-  typedef std::list<LogicalModule> LogicalModuleList;
-
-public:
-
-  typedef typename BaseLayerHandleList::iterator BaseLayerHandleIterator;
-  typedef typename LogicalModuleList::iterator LogicalModuleHandle;
-
-  MullLogicalDylib(CompileLayer &BaseLayer) : BaseLayer(BaseLayer) {}
-
-  ~MullLogicalDylib() {
-    for (auto &LM : LogicalModules)
-      for (auto BLH : LM.BaseLayerHandles)
-        BaseLayer.removeModuleSet(BLH);
-  }
-
-  // If possible, remove this and ~LogicalDylib once the work in the dtor is
-  // moved to members (eg: self-unregistering base layer handles).
-  MullLogicalDylib(MullLogicalDylib &&RHS)
-  : BaseLayer(std::move(RHS.BaseLayer)),
-    LogicalModules(std::move(RHS.LogicalModules)),
-    DylibResources(std::move(RHS.DylibResources)) {}
-
-  LogicalModuleHandle createLogicalModule() {
-    LogicalModules.push_back(LogicalModule());
-    return std::prev(LogicalModules.end());
-  }
-
-  void addToLogicalModule(LogicalModuleHandle LMH,
-                          BaseLayerModuleSetHandleT BaseLayerHandle) {
-    LMH->BaseLayerHandles.push_back(BaseLayerHandle);
-  }
-
-  LogicalModuleResources& getLogicalModuleResources(LogicalModuleHandle LMH) {
-    return LMH->Resources;
-  }
-
-  BaseLayerHandleIterator moduleHandlesBegin(LogicalModuleHandle LMH) {
-    return LMH->BaseLayerHandles.begin();
-  }
-
-  BaseLayerHandleIterator moduleHandlesEnd(LogicalModuleHandle LMH) {
-    return LMH->BaseLayerHandles.end();
-  }
-
-  llvm::orc::JITSymbol findSymbolInLogicalModule(LogicalModuleHandle LMH,
-                                      const std::string &Name,
-                                      bool ExportedSymbolsOnly) {
-
-    if (auto StubSym = LMH->Resources.findSymbol(Name, ExportedSymbolsOnly))
-      return StubSym;
-
-    for (auto BLH : LMH->BaseLayerHandles)
-      if (auto Symbol = BaseLayer.findSymbolIn(BLH, Name, ExportedSymbolsOnly))
-        return Symbol;
-    return nullptr;
-  }
-
-  llvm::orc::JITSymbol findSymbolInternally(LogicalModuleHandle LMH,
-                                 const std::string &Name) {
-    if (auto Symbol = findSymbolInLogicalModule(LMH, Name, false))
-      return Symbol;
-
-    for (auto LMI = LogicalModules.begin(), LME = LogicalModules.end();
-         LMI != LME; ++LMI) {
-      if (LMI != LMH)
-        if (auto Symbol = findSymbolInLogicalModule(LMI, Name, false))
-          return Symbol;
-    }
-
-    return nullptr;
-  }
-
-  llvm::orc::JITSymbol findSymbol(const std::string &Name, bool ExportedSymbolsOnly) {
-    for (auto LMI = LogicalModules.begin(), LME = LogicalModules.end();
-         LMI != LME; ++LMI)
-      if (auto Sym = findSymbolInLogicalModule(LMI, Name, ExportedSymbolsOnly))
-        return Sym;
-    return nullptr;
-  }
-  
-  LogicalDylibResources& getDylibResources() { return DylibResources; }
-  
-protected:
-  CompileLayer BaseLayer;
-  LogicalModuleList LogicalModules;
-  LogicalDylibResources DylibResources;
-};
-
 class MullJIT {
 private:
+
+  typedef typename CompileLayer::ModuleSetHandleT BaseLayerModuleSetHandleT;
+  typedef std::vector<BaseLayerModuleSetHandleT> BaseLayerHandleList;
 
   llvm::orc::SimpleCompiler compiler;
   LinkingLayer linkingLayer;
@@ -177,8 +72,6 @@ private:
   createLambdaMaterializer(MaterializerFtor M) {
     return LambdaMaterializer<MaterializerFtor>(std::move(M));
   }
-
-  typedef typename CompileLayer::ModuleSetHandleT BaseLayerModuleSetHandleT;
 
   // Provide type-erasure for the Modules and MemoryManagers.
   template <typename ResourceT>
@@ -270,14 +163,24 @@ private:
     ModuleAdderFtor ModuleAdder;
   };
 
-  typedef MullLogicalDylib<LogicalModuleResources,
-  LogicalDylibResources> CODLogicalDylib;
+  struct LogicalModule {
+    // Make this move-only to ensure they don't get duplicated across moves of
+    // LogicalDylib or anything like that.
+    LogicalModule(LogicalModule &&RHS)
+    : Resources(std::move(RHS.Resources)),
+    BaseLayerHandles(std::move(RHS.BaseLayerHandles)) {}
 
-  typedef typename CODLogicalDylib::LogicalModuleHandle LogicalModuleHandle;
+    LogicalModule() = default;
+    LogicalModuleResources Resources;
+    BaseLayerHandleList BaseLayerHandles;
+  };
+
+  std::list<LogicalModule> logicalModules;
 public:
 
   /// @brief Construct a compile-on-demand layer instance.
   MullJIT(llvm::TargetMachine &machine);
+  ~MullJIT();
 
   void addModuleSet(std::vector<llvm::Module *> &modules,
                     std::unique_ptr<SectionMemoryManager> memoryManager,
@@ -307,11 +210,11 @@ private:
 
   static std::string mangle(llvm::StringRef Name, const llvm::DataLayout &DL);
 
-  llvm::orc::TargetAddress extractAndCompile(LogicalModuleHandle LMH,
+  llvm::orc::TargetAddress extractAndCompile(std::list<LogicalModule>::iterator LMH,
                                              Function &F,
                                              uint64_t functionIndex);
 
-  BaseLayerModuleSetHandleT emitFunction(LogicalModuleHandle LMH,
+  BaseLayerModuleSetHandleT emitFunction(std::list<LogicalModule>::iterator LMH,
                                          Function &function,
                                          uint64_t functionIndex);
   void tryToApplyMutation(Module *module, Function *originalFunction, Function *function);
@@ -321,10 +224,37 @@ private:
   std::unique_ptr<llvm::orc::JITCompileCallbackManager> callbackManager;
   std::function<std::unique_ptr<llvm::orc::IndirectStubsManager>()> stubsManagerBuilder;
 
-  CODLogicalDylib logicalDylib;
   std::vector<CallTreeFunction> functions;
   DynamicCallTree dynamicCallTree;
   MutationPoint *currentMutationPoint;
+  LogicalDylibResources logicalDylibResources;
+
+  llvm::orc::JITSymbol findSymbolInLogicalModule(std::list<LogicalModule>::iterator LMH,
+                                                 const std::string &Name,
+                                                 bool ExportedSymbolsOnly) {
+    if (auto StubSym = LMH->Resources.findSymbol(Name, ExportedSymbolsOnly))
+      return StubSym;
+
+    for (auto BLH : LMH->BaseLayerHandles)
+      if (auto Symbol = compileLayer.findSymbolIn(BLH, Name, ExportedSymbolsOnly))
+        return Symbol;
+    return nullptr;
+  }
+
+  llvm::orc::JITSymbol findSymbolInternally(std::list<LogicalModule>::iterator LMH,
+                                            const std::string &Name) {
+    if (auto Symbol = findSymbolInLogicalModule(LMH, Name, false))
+      return Symbol;
+
+    for (auto LMI = logicalModules.begin(), LME = logicalModules.end();
+         LMI != LME; ++LMI) {
+      if (LMI != LMH)
+        if (auto Symbol = findSymbolInLogicalModule(LMI, Name, false))
+          return Symbol;
+    }
+
+    return nullptr;
+  }
 };
 
 }
--- lib/MullJIT.cpp
+++ lib/MullJIT.cpp
@@ -40,23 +40,27 @@ MullJIT::MullJIT(llvm::TargetMachine &machine)
       compileLayer(linkingLayer, compiler),
       callbackManager(llvm::orc::createLocalCompileCallbackManager(machine.getTargetTriple(), 0)),
       stubsManagerBuilder(llvm::orc::createLocalIndirectStubsManagerBuilder(machine.getTargetTriple())),
-      logicalDylib(compileLayer),
       dynamicCallTree(functions) {
   CallTreeFunction phonyRoot(nullptr);
   functions.push_back(phonyRoot);
 }
 
+MullJIT::~MullJIT() {
+  for (auto &LM : logicalModules) {
+    for (auto BLH : LM.BaseLayerHandles) {
+      compileLayer.removeModuleSet(BLH);
+    }
+  }
+}
+
 void MullJIT::addModuleSet(std::vector<llvm::Module *> &modules, std::unique_ptr<SectionMemoryManager> memoryManager,
                            std::unique_ptr<RuntimeDyld::SymbolResolver> resolver) {
-  auto &LDResources = logicalDylib.getDylibResources();
-
-  LDResources.ExternalSymbolResolver = std::move(resolver);
+  logicalDylibResources.ExternalSymbolResolver = std::move(resolver);
 
   auto &MemMgrRef = *memoryManager;
-  LDResources.MemMgr =
-      wrapOwnership<RuntimeDyld::MemoryManager>(std::move(memoryManager));
+  logicalDylibResources.MemMgr = wrapOwnership<RuntimeDyld::MemoryManager>(std::move(memoryManager));
 
-  LDResources.ModuleAdder =
+  logicalDylibResources.ModuleAdder =
       [&MemMgrRef](CompileLayer &B, std::unique_ptr<Module> M,
                    std::unique_ptr<RuntimeDyld::SymbolResolver> R) {
         std::vector<std::unique_ptr<Module>> Ms;
@@ -65,16 +69,19 @@ void MullJIT::addModuleSet(std::vector<llvm::Module *> &modules, std::unique_ptr
       };
 
   // Process each of the modules in this module set.
-  for (auto &M : modules) {
-    addLogicalModule(std::move(M));
+  for (auto module : modules) {
+    addLogicalModule(module);
   }
 
   prepareForExecution();
 }
 
 llvm::orc::JITSymbol MullJIT::findSymbol(StringRef Name, bool ExportedSymbolsOnly) {
-  if (auto Symbol = logicalDylib.findSymbol(Name, ExportedSymbolsOnly))
-    return Symbol;
+  for (auto LMI = logicalModules.begin(), LME = logicalModules.end(); LMI != LME; ++LMI) {
+    if (auto Sym = findSymbolInLogicalModule(LMI, Name, ExportedSymbolsOnly)) {
+      return Sym;
+    }
+  }
   return compileLayer.findSymbol(Name, ExportedSymbolsOnly);
 }
 
@@ -107,11 +114,10 @@ void MullJIT::addLogicalModule(llvm::Module *module) {
   // ensure that everything will resolve properly after we partition SrcM.
   orc::makeAllSymbolsExternallyAccessible(*module);
 
-  // Create a logical module handle for SrcM within the logical dylib.
-  auto LMH = logicalDylib.createLogicalModule();
-  auto &LMResources = logicalDylib.getLogicalModuleResources(LMH);
-
-  LMResources.SourceModule = module;
+  logicalModules.push_back(LogicalModule());
+  std::list<LogicalModule>::iterator logicalModuleIterator = std::prev(logicalModules.end());
+  LogicalModuleResources &resources = logicalModuleIterator->Resources;
+  resources.SourceModule = module;
 
   // Create stub functions.
   const DataLayout &DL = module->getDataLayout();
@@ -120,10 +126,10 @@ void MullJIT::addLogicalModule(llvm::Module *module) {
     for (auto &F : *module) {
       // Skip declarations.
 
-      if (F.isDeclaration())
+      if (F.isDeclaration()) {
         continue;
-
-      LMResources.StubsToClone.insert(&F);
+      }
+      resources.StubsToClone.insert(&F);
 
       // Create a callback, associate it with the stub for the function,
       // and set the compile action to compile the partition containing the
@@ -136,14 +142,14 @@ void MullJIT::addLogicalModule(llvm::Module *module) {
       CallTreeFunction callTreeFunction(&F);
       uint64_t index = functions.size();
       functions.push_back(callTreeFunction);
-      CCInfo.setCompileAction([this, LMH, &F, index]() {
-        return this->extractAndCompile(LMH, F, index);
+      CCInfo.setCompileAction([this, logicalModuleIterator, &F, index]() {
+        return this->extractAndCompile(logicalModuleIterator, F, index);
       });
     }
 
-    LMResources.StubsMgr = stubsManagerBuilder();
+    resources.StubsMgr = stubsManagerBuilder();
 //      LMResources.StubsMgr = make_unique<orc::LocalIndirectStubsManager<orc::OrcX86_64_SysV>>();
-    auto EC = LMResources.StubsMgr->createStubs(StubInits);
+    auto EC = resources.StubsMgr->createStubs(StubInits);
     (void) EC;
     // FIXME: This should be propagated back to the user. Stub creation may
     //        fail for remote JITs.
@@ -178,7 +184,7 @@ void MullJIT::addLogicalModule(llvm::Module *module) {
   // Initializers may refer to functions declared (but not defined) in this
   // module. Build a materializer to clone decls on demand.
   auto Materializer = createLambdaMaterializer(
-      [this, &GVsM, &LMResources](Value *V) -> Value * {
+      [this, &GVsM, &resources](Value *V) -> Value * {
         if (auto *F = dyn_cast<Function>(V)) {
           // Decls in the original module just get cloned.
           if (F->isDeclaration())
@@ -189,7 +195,7 @@ void MullJIT::addLogicalModule(llvm::Module *module) {
           // instead.
           const DataLayout &DL = GVsM->getDataLayout();
           std::string FName = mangle(F->getName(), DL);
-          auto StubSym = LMResources.StubsMgr->findStub(FName, false);
+          auto StubSym = resources.StubsMgr->findStub(FName, false);
           unsigned PtrBitWidth = DL.getPointerTypeSizeInBits(F->getType());
           ConstantInt *StubAddr =
               ConstantInt::get(GVsM->getContext(),
@@ -221,21 +227,21 @@ void MullJIT::addLogicalModule(llvm::Module *module) {
 
   // Build a resolver for the globals module and add it to the base layer.
   auto GVsResolver = orc::createLambdaResolver(
-      [this, LMH](const std::string &Name) {
-        auto &LMResources = logicalDylib.getLogicalModuleResources(LMH);
-        if (auto Sym = LMResources.StubsMgr->findStub(Name, false))
+      [this, logicalModuleIterator](const std::string &Name) {
+        auto &resources = logicalModuleIterator->Resources;
+        if (auto Sym = resources.StubsMgr->findStub(Name, false))
           return Sym.toRuntimeDyldSymbol();
-        auto &LDResolver = logicalDylib.getDylibResources().ExternalSymbolResolver;
+        auto &LDResolver = logicalDylibResources.ExternalSymbolResolver;
         return LDResolver->findSymbolInLogicalDylib(Name);
       },
       [this](const std::string &Name) {
-        auto &LDResolver = logicalDylib.getDylibResources().ExternalSymbolResolver;
+        auto &LDResolver = logicalDylibResources.ExternalSymbolResolver;
         return LDResolver->findSymbol(Name);
       });
 
-  auto GVsH = logicalDylib.getDylibResources().ModuleAdder(compileLayer, std::move(GVsM),
-                                                           std::move(GVsResolver));
-  logicalDylib.addToLogicalModule(LMH, GVsH);
+  auto GVsH = logicalDylibResources.ModuleAdder(compileLayer, std::move(GVsM),
+                                                 std::move(GVsResolver));
+  logicalModuleIterator->BaseLayerHandles.push_back(GVsH);
 }
 
 std::string MullJIT::mangle(llvm::StringRef Name, const llvm::DataLayout &DL) {
@@ -247,11 +253,11 @@ std::string MullJIT::mangle(llvm::StringRef Name, const llvm::DataLayout &DL) {
   return MangledName;
 }
 
-llvm::orc::TargetAddress MullJIT::extractAndCompile(MullJIT::LogicalModuleHandle LMH,
+llvm::orc::TargetAddress MullJIT::extractAndCompile(std::list<LogicalModule>::iterator LMH,
                                                     Function &F,
                                                     uint64_t functionIndex) {
-  auto &LMResources = logicalDylib.getLogicalModuleResources(LMH);
-  Module &module = *LMResources.SourceModule;
+  LogicalModuleResources &resources = LMH->Resources;
+  Module &module = *resources.SourceModule;
 
   // If F is a declaration we must already have compiled it.
   if (F.isDeclaration())
@@ -268,17 +274,17 @@ llvm::orc::TargetAddress MullJIT::extractAndCompile(MullJIT::LogicalModuleHandle
   llvm::orc::TargetAddress FnBodyAddr = FnBodySym.getAddress();
 
   // Update the function body pointer for the stub.
-  if (auto EC = LMResources.StubsMgr->updatePointer(FnName, FnBodyAddr))
+  if (auto EC = resources.StubsMgr->updatePointer(FnName, FnBodyAddr))
     return 0;
 
   return FnBodyAddr;
 }
 
-MullJIT::BaseLayerModuleSetHandleT MullJIT::emitFunction(MullJIT::LogicalModuleHandle LMH,
+MullJIT::BaseLayerModuleSetHandleT MullJIT::emitFunction(std::list<LogicalModule>::iterator LMH,
                                                          Function &function,
                                                          uint64_t functionIndex) {
-  auto &LMResources = logicalDylib.getLogicalModuleResources(LMH);
-  Module &module = *LMResources.SourceModule;
+  LogicalModuleResources &resources = LMH->Resources;
+  Module &module = *resources.SourceModule;
 
   // Create the module.
   std::string NewName = module.getName();
@@ -289,14 +295,14 @@ MullJIT::BaseLayerModuleSetHandleT MullJIT::emitFunction(MullJIT::LogicalModuleH
   M->setDataLayout(module.getDataLayout());
   ValueToValueMapTy VMap;
 
-  auto Materializer = createLambdaMaterializer([this, &LMResources, &M,
+  auto Materializer = createLambdaMaterializer([this, &resources, &M,
                                                    &VMap, functionIndex](Value *V) -> Value * {
     if (auto *GV = dyn_cast<GlobalVariable>(V))
       return orc::cloneGlobalVariableDecl(*M, *GV);
 
     if (auto *F = dyn_cast<Function>(V)) {
       // Check whether we want to clone an available_externally definition.
-      if (!LMResources.StubsToClone.count(F))
+      if (!resources.StubsToClone.count(F))
         return orc::cloneFunctionDecl(*M, *F);
 
       // Ok - we want an inlinable stub. For that to work we need a decl
@@ -335,20 +341,20 @@ MullJIT::BaseLayerModuleSetHandleT MullJIT::emitFunction(MullJIT::LogicalModuleH
   // Create memory manager and symbol resolver.
   auto Resolver = orc::createLambdaResolver(
       [this, LMH](const std::string &Name) {
-        if (auto Sym = logicalDylib.findSymbolInternally(LMH, Name))
+        if (auto Sym = this->findSymbolInternally(LMH, Name))
           return Sym.toRuntimeDyldSymbol();
-        auto &LDResolver = logicalDylib.getDylibResources().ExternalSymbolResolver;
+        auto &LDResolver = logicalDylibResources.ExternalSymbolResolver;
         return LDResolver->findSymbolInLogicalDylib(Name);
       },
       [this](const std::string &Name) {
-        auto &LDResolver = logicalDylib.getDylibResources().ExternalSymbolResolver;
+        auto &LDResolver = logicalDylibResources.ExternalSymbolResolver;
         return LDResolver->findSymbol(Name);
       });
 
   insertCallTreeCallbacks(&module, &function, *M->getFunction(function.getName()), functionIndex);
 
-  return logicalDylib.getDylibResources().ModuleAdder(compileLayer, std::move(M),
-                                                      std::move(Resolver));
+  return logicalDylibResources.ModuleAdder(compileLayer, std::move(M),
+                                           std::move(Resolver));
 }
 
 void MullJIT::insertCallTreeCallbacks(Module *module, Function *originalFunction, Function &function, uint64_t index) {
