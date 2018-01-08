4d2c3c440a1cededa004952f95826805b0311160
AlexDenisov <1101.debian@gmail.com>
2018-01-08 09:26:24 +0100

    Remove unused artefacts

--- include/GoogleTest/GoogleTestRunner.h
+++ include/GoogleTest/GoogleTestRunner.h
@@ -14,7 +14,6 @@
 namespace llvm {
 
 class Function;
-class Module;
 
 }
 
@@ -28,7 +27,6 @@ class GoogleTestRunner : public TestRunner {
   std::string fGoogleTestInit;
   std::string fGoogleTestInstance;
   std::string fGoogleTestRun;
-  llvm::orc::ObjectLinkingLayer<>::ObjSetHandleT handle;
 public:
 
   GoogleTestRunner(llvm::TargetMachine &machine);
