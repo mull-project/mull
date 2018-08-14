22266dc582fe2118cf7aaa9ef7537ae603301df5
AlexDenisov <1101.debian@gmail.com>
2018-08-14 20:03:40 +0200

    Extract ModuleLoadingTask into a separate file

--- lib/ModuleLoader.cpp
+++ lib/ModuleLoader.cpp
@@ -3,6 +3,7 @@
 #include "Logger.h"
 #include "LLVMCompatibility.h"
 #include "Parallelization/Parallelization.h"
+#include "Parallelization/Tasks/ModuleLoadingTask.h"
 
 #include <llvm/AsmParser/Parser.h>
 #include <llvm/IR/LLVMContext.h>
@@ -13,29 +14,9 @@
 
 #include <fstream>
 #include <iostream>
-#include <thread>
 
 using namespace llvm;
 using namespace mull;
-
-class ModuleLoaderTask {
-public:
-  using In = const std::vector<std::string>;
-  using Out = std::vector<std::unique_ptr<MullModule>>;
-  using iterator = In::const_iterator;
-  ModuleLoaderTask(LLVMContext &context, ModuleLoader &loader)
-    : context(context), loader(loader) {}
-  void operator() (iterator begin, iterator end, Out &storage, progress_counter &counter) {
-    for (auto it = begin; it != end; ++it, counter.increment()) {
-      auto module = loader.loadModuleAtPath(*it, context);
-      if (module != nullptr) {
-        storage.push_back(std::move(module));
-      }
-    }
-  }
-  LLVMContext &context;
-  ModuleLoader &loader;
-};
 
 static std::string MD5HashFromBuffer(StringRef buffer) {
   MD5 Hasher;
@@ -73,14 +54,14 @@ ModuleLoader::loadModulesFromBitcodeFileList(const std::vector<std::string> &bit
   std::vector<std::unique_ptr<MullModule>> modules;
 
   int workers = std::thread::hardware_concurrency();
-  std::vector<ModuleLoaderTask> tasks;
+  std::vector<ModuleLoadingTask> tasks;
   for (int i = 0; i < workers; i++) {
     auto context = llvm::make_unique<LLVMContext>();
     tasks.emplace_back(*context, *this);
     contexts.push_back(std::move(context));
   }
 
-  TaskExecutor<ModuleLoaderTask> loader("Loading bitcode", bitcodeFileList, modules, tasks);
+  TaskExecutor<ModuleLoadingTask> loader("Loading bitcode", bitcodeFileList, modules, tasks);
   loader.execute();
 
   return modules;
