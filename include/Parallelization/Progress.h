a376bdd2f08beeaa890078ae352c926150e8d631
AlexDenisov <1101.debian@gmail.com>
2018-08-17 18:17:54 +0200

    Add infrastructure for parallelization

---
+++ include/Parallelization/Progress.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <cstddef>
+#include <atomic>
+#include <string>
+#include <vector>
+
+namespace llvm {
+class raw_ostream;
+}
+
+namespace mull {
+class progress_counter {
+public:
+  using CounterType = size_t;
+
+  progress_counter() = default;
+  progress_counter(const progress_counter &v);
+  void increment();
+  CounterType get();
+private:
+  std::atomic<CounterType> value;
+};
+
+class progress_reporter {
+public:
+  progress_reporter(std::string &name,
+                    std::vector<progress_counter> &counters,
+                    progress_counter::CounterType total,
+                    size_t workers,
+                    llvm::raw_ostream &stream);
+
+  void operator()();
+private:
+  void printProgress(progress_counter::CounterType current,
+                     progress_counter::CounterType total);
+  void backspace();
+
+  std::vector<progress_counter> &counters;
+  llvm::raw_ostream &stream;
+  progress_counter::CounterType total;
+  std::string backspaces;
+};
+}
