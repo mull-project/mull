b69dced609b2289ad805364f724cd4c44195f03c
myfreeweb <greg@unrelenting.technology>
2018-10-06 20:20:06 +0300

    Use i8 instead of void for trampoline pointers
    Creating void pointers is forbidden by LLVM assertions ("Invalid type for pointer element!").
    Clang uses pointers to 8-bit integers to represent void pointers, so let's do the same.
    
    Why minus one ->getPointerTo()? For some reason injectInstrumentationInfoPointer was generating a triple pointer, which tripped a different assertion ("Calling a function with a bad signature!")

--- lib/Instrumentation/Callbacks.cpp
+++ lib/Instrumentation/Callbacks.cpp
@@ -31,7 +31,7 @@ extern "C" void mull_leaveFunction(void **trampoline, uint32_t functionIndex) {
 Value *Callbacks::injectInstrumentationInfoPointer(Module *module,
                                                    const char *variableName) {
   auto &context = module->getContext();
-  auto trampolineType = Type::getVoidTy(context)->getPointerTo()->getPointerTo();
+  auto trampolineType = Type::getInt8Ty(context)->getPointerTo();
   return module->getOrInsertGlobal(variableName, trampolineType);
 }
 
@@ -51,7 +51,7 @@ void Callbacks::injectCallbacks(llvm::Function *function,
                                 Value *offset) {
   auto &context = function->getParent()->getContext();
   auto intType = Type::getInt32Ty(context);
-  auto trampolineType = Type::getVoidTy(context)->getPointerTo()->getPointerTo();
+  auto trampolineType = Type::getInt8Ty(context)->getPointerTo()->getPointerTo();
   auto voidType = Type::getVoidTy(context);
   std::vector<Type *> parameterTypes({trampolineType, intType});
 
