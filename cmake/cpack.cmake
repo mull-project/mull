d225fbef81c9eef49175c969e45b6ef107587b97
AlexDenisov <1101.debian@gmail.com>
2019-04-02 00:03:00 +0200

    Include more info into the package name

--- cmake/cpack.cmake
+++ cmake/cpack.cmake
@@ -8,6 +8,7 @@ set (CPACK_PACKAGE_HOMEPAGE_URL ${PROJECT_HOMEPAGE_URL})
 set (CPACK_PACKAGE_VERSION_MAJOR ${PROJECT_VERSION_MAJOR})
 set (CPACK_PACKAGE_VERSION_MINOR ${PROJECT_VERSION_MINOR})
 set (CPACK_PACKAGE_VERSION_PATCH ${PROJECT_VERSION_PATCH})
+set (CPACK_PACKAGE_VERSION ${PROJECT_VERSION})
 
 set (CPACK_RESOURCE_FILE_LICENSE "${CMAKE_CURRENT_BINARY_DIR}/LICENSE.txt")
 configure_file(${CMAKE_SOURCE_DIR}/LICENSE ${CPACK_RESOURCE_FILE_LICENSE})
@@ -16,30 +17,46 @@ set (CPACK_RESOURCE_FILE_README "${CMAKE_SOURCE_DIR}/cmake/packaging/ReadMe.txt"
 set (CPACK_RESOURCE_FILE_WELCOME "${CMAKE_SOURCE_DIR}/cmake/packaging/Welcome.txt")
 
 if (APPLE)
-  include(cmake/packaging/cpack.PackageMaker.cmake)
+  set (CPACK_SYSTEM_NAME "macOS")
+  execute_process(
+    COMMAND sw_vers -productVersion
+    OUTPUT_VARIABLE CPACK_SYSTEM_VERSION
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+  )
+
 else()
   if(EXISTS /etc/os-release)
     file(READ /etc/os-release OS_RELEASE)
-    string(REGEX MATCH "ID=([A-Za-z]+)" match ${OS_RELEASE}})
-    if (NOT match)
-      message(WARNING "Cannot find distro ID in the /etc/os-release")
-      return()
-    endif()
+    string(REGEX REPLACE "\r?\n" ";" LINES "${OS_RELEASE}")
 
-    set (CPACK_SYSTEM_NAME ${CMAKE_MATCH_1})
+    foreach(LINE ${LINES})
+      string(REGEX MATCH "^ID=\"?([A-Za-z0-9._-]+)\"?" match ${LINE}})
+      if (match)
+        set (CPACK_SYSTEM_NAME ${CMAKE_MATCH_1})
+      endif()
 
-    if (${CPACK_SYSTEM_NAME} STREQUAL "debian")
-      include(cmake/packaging/cpack.DEB-debian.cmake)
-    elseif(${CPACK_SYSTEM_NAME} STREQUAL "ubuntu")
-      include(cmake/packaging/cpack.DEB-ubuntu.cmake)
-    elseif(${CPACK_SYSTEM_NAME} STREQUAL "fedora")
-      include(cmake/packaging/cpack.RPM-fedora.cmake)
-    else()
-      message(WARNING "Mull cannot generate package for ${CPACK_SYSTEM_NAME} yet.")
-    endif()
+      string(REGEX MATCH "^VERSION_ID=\"?([A-Za-z0-9._-]+)\"?" match ${LINE}})
+      if (match)
+        set (CPACK_SYSTEM_VERSION ${CMAKE_MATCH_1})
+      endif()
+    endforeach()
 
   endif()
 endif()
+
+if (${CPACK_SYSTEM_NAME} STREQUAL "macOS")
+  include(cmake/packaging/cpack.PackageMaker.cmake)
+elseif (${CPACK_SYSTEM_NAME} STREQUAL "debian")
+  include(cmake/packaging/cpack.DEB-debian.cmake)
+elseif(${CPACK_SYSTEM_NAME} STREQUAL "ubuntu")
+  include(cmake/packaging/cpack.DEB-ubuntu.cmake)
+elseif(${CPACK_SYSTEM_NAME} STREQUAL "fedora")
+  include(cmake/packaging/cpack.RPM-fedora.cmake)
+else()
+  message(WARNING "Mull cannot generate package for ${CPACK_SYSTEM_NAME} yet.")
+endif()
+
+set (CPACK_PACKAGE_FILE_NAME "${CPACK_PACKAGE_NAME}-${CPACK_PACKAGE_VERSION}-LLVM-${LLVM_VERSION_MAJOR}.${LLVM_VERSION_MINOR}-${CPACK_SYSTEM_NAME}-${CPACK_SYSTEM_VERSION}")
 
 include(CPack)
 
