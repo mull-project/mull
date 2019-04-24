a67e4ee955bee5a9f3cae2abbe50ef9322b6cd3b
AlexDenisov <1101.debian@gmail.com>
2019-04-24 22:34:28 +0200

    Add target to validate mutations
    Closes #451.

--- cmake/functions.cmake
+++ cmake/functions.cmake
@@ -27,3 +27,28 @@ macro(add_mull_executable)
   )
 endmacro()
 
+macro(add_mull_internal_executable)
+  set (prefix local)
+  set (optionArguments )
+  set (singleValueArguments NAME)
+  set (multipleValueArguments SOURCES LINK_WITH)
+
+  cmake_parse_arguments(${prefix}
+    "${optionArguments}"
+    "${singleValueArguments}"
+    "${multipleValueArguments}"
+    ${ARGN}
+  )
+
+  add_executable(${local_NAME} ${local_SOURCES})
+  target_link_libraries(${local_NAME} ${local_LINK_WITH})
+
+  set_target_properties(${local_NAME} PROPERTIES
+    LINK_FLAGS ${MULL_LINK_FLAGS}
+    COMPILE_FLAGS ${MULL_CXX_FLAGS}
+  )
+  target_include_directories(${local_NAME} PUBLIC
+    ${MULL_INCLUDE_DIRS}
+  )
+endmacro()
+
