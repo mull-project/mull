7b997007513faa10c33940b9fcb357dd8297f46a
AlexDenisov <1101.debian@gmail.com>
2019-05-01 08:46:17 +0200

    Automate release engineering

---
+++ infrastructure/release.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+mkdir -p packages
+
+function prepare_package () {
+printf "Preparing package for $1... "
+  export LLVM_VERSION=$2
+  vagrant up $1 --provision 2> ./packages/$1.err.log > ./packages/$1.out.log
+  vagrant destroy -f $1 2>> ./packages/$1.err.log >> ./packages/$1.out.log
+  printf "Done.\n"
+}
+
+prepare_package debian 6.0.0
+prepare_package freebsd 8.0.0
+prepare_package ubuntu 8.0.0
+
