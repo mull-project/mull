6c11c8b3bc6b99151505c6b85994c95d0c2b4798
Stanislav Pankevich <s.pankevich@gmail.com>
2019-06-14 23:31:58 +0200

    Reporters: extract SourceManager to a separate class

---
+++ include/mull/Reporters/SourceManager.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "mull/SourceLocation.h"
+
+#include <map>
+#include <utility>
+#include <vector>
+
+namespace mull {
+
+struct LineOffset {
+  FILE *file;
+  std::vector<uint32_t> offsets;
+  LineOffset(FILE *file, std::vector<uint32_t> offsets)
+      : file(file), offsets(std::move(offsets)) {}
+};
+
+class SourceManager {
+public:
+  std::string getLine(const SourceLocation &location);
+
+  ~SourceManager();
+
+private:
+  std::map<std::string, LineOffset> lineOffsets;
+
+  LineOffset &getLineOffset(const SourceLocation &location);
+};
+
+} // namespace mull
