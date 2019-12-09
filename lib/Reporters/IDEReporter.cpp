bb6971361ca914fd1e203006ee12e9a9f7fe7a34
Stanislav Pankevich <s.pankevich@gmail.com>
2019-12-09 23:16:04 +0100

    IDEReporter: print killed mutants when -ide-reporter-show-killed is provided

--- lib/Reporters/IDEReporter.cpp
+++ lib/Reporters/IDEReporter.cpp
@@ -9,7 +9,6 @@
 #include <set>
 #include <string>
 #include <utility>
-#include <vector>
 
 using namespace mull;
 
@@ -17,15 +16,18 @@ static bool mutantSurvived(const ExecutionStatus &status) {
   return status == ExecutionStatus::Passed;
 }
 
-static void printSurvivedMutant(SourceManager &sourceManager,
-                                const MutationPoint &mutant) {
+static void printMutant(SourceManager &sourceManager, const MutationPoint &mutant, bool survived) {
   auto &sourceLocation = mutant.getSourceLocation();
   assert(!sourceLocation.isNull() && "Debug information is missing?");
+
+  const std::string status = survived ? "Survived" : "Killed";
   Logger::info() << sourceLocation.filePath << ":" << sourceLocation.line << ":"
-                 << sourceLocation.column
-                 << ": warning: " << mutant.getDiagnostics() << "\n";
+                 << sourceLocation.column << ": warning: " << status << ": "
+                 << mutant.getDiagnostics() << "\n";
+
   auto line = sourceManager.getLine(sourceLocation);
   assert(sourceLocation.column < line.size());
+
   std::string caret(sourceLocation.column, ' ');
   for (size_t index = 0; index < sourceLocation.column; index++) {
     if (line[index] == '\t') {
@@ -38,37 +40,91 @@ static void printSurvivedMutant(SourceManager &sourceManager,
   Logger::info() << caret << "\n";
 }
 
+IDEReporter::IDEReporter(bool showKilled) : showKilled(showKilled) {}
+
 void IDEReporter::reportResults(const Result &result, const Metrics &metrics) {
+  if (showKilled) {
+    reportAllMutants(result, metrics);
+  } else {
+    reportSurvivedMutants(result, metrics);
+  }
+}
+
+void IDEReporter::reportSurvivedMutants(const Result &result, const Metrics &metrics) {
+  if (result.getMutationPoints().empty()) {
+    Logger::info() << "No mutants found. Mutation score: infinitely high\n";
+    return;
+  }
+
+  std::set<MutationPoint *> survivedMutants;
+  for (auto &mutationResult : result.getMutationResults()) {
+    auto mutant = mutationResult->getMutationPoint();
+    auto &executionResult = mutationResult->getExecutionResult();
+
+    if (mutantSurvived(executionResult.status)) {
+      survivedMutants.insert(mutant);
+    }
+  }
+
+  Logger::info() << "\nSurvived mutants (" << survivedMutants.size() << "/"
+                 << result.getMutationPoints().size() << "):\n\n";
+
+  SourceManager sourceManager;
+  for (auto &mutant : survivedMutants) {
+    printMutant(sourceManager, *mutant, true);
+  }
+  if (!survivedMutants.empty()) {
+    Logger::info() << "\n";
+  }
+
+  size_t killedMutantsCount = result.getMutationPoints().size() - survivedMutants.size();
+  auto rawScore = double(killedMutantsCount) / double(result.getMutationPoints().size());
+  auto score = uint(rawScore * 100);
+  Logger::info() << "Mutation score: " << score << "%\n";
+}
+
+void IDEReporter::reportAllMutants(const Result &result, const Metrics &metrics) {
   if (result.getMutationPoints().empty()) {
     Logger::info() << "No mutants found. Mutation score: infinitely high\n";
     return;
   }
 
   std::set<MutationPoint *> killedMutants;
+  std::set<MutationPoint *> survivedMutants;
   for (auto &mutationResult : result.getMutationResults()) {
     auto mutant = mutationResult->getMutationPoint();
     auto &executionResult = mutationResult->getExecutionResult();
 
     if (!mutantSurvived(executionResult.status)) {
       killedMutants.insert(mutant);
+    } else {
+      survivedMutants.insert(mutant);
     }
   }
 
-  auto survivedMutantsCount =
-      result.getMutationPoints().size() - killedMutants.size();
+  auto survivedMutantsCount = result.getMutationPoints().size() - killedMutants.size();
+  auto killedMutantsCount = killedMutants.size();
+
+  SourceManager sourceManager;
+
+  Logger::info() << "\nKilled mutants (" << killedMutantsCount << "/"
+                 << result.getMutationPoints().size() << "):\n\n";
+
+  for (auto &mutant : killedMutants) {
+    printMutant(sourceManager, *mutant, false);
+  }
 
   Logger::info() << "\nSurvived mutants (" << survivedMutantsCount << "/"
                  << result.getMutationPoints().size() << "):\n\n";
 
-  SourceManager sourceManager;
-  for (auto mutant : result.getMutationPoints()) {
-    if (killedMutants.find(mutant) == killedMutants.end()) {
-      printSurvivedMutant(sourceManager, *mutant);
-    }
+  for (auto &mutant : survivedMutants) {
+    printMutant(sourceManager, *mutant, true);
+  }
+  if (!survivedMutants.empty()) {
+    Logger::info() << "\n";
   }
 
-  auto rawScore =
-      double(killedMutants.size()) / double(result.getMutationPoints().size());
+  auto rawScore = double(killedMutants.size()) / double(result.getMutationPoints().size());
   auto score = uint(rawScore * 100);
   Logger::info() << "Mutation score: " << score << "%\n";
 }
