d1db0ea5daa1036d6977265eb966f885f3d9fa79
Stanislav Pankevich <s.pankevich@gmail.com>
2019-12-15 18:47:06 +0100

    IDE reporter: simplify control flow

--- lib/Reporters/IDEReporter.cpp
+++ lib/Reporters/IDEReporter.cpp
@@ -42,68 +42,18 @@ static void printMutant(SourceManager &sourceManager, const MutationPoint &mutan
 IDEReporter::IDEReporter(bool showKilled) : showKilled(showKilled) {}
 
 void IDEReporter::reportResults(const Result &result, const Metrics &metrics) {
-  if (showKilled) {
-    reportAllMutants(result, metrics);
-  } else {
-    reportSurvivedMutants(result, metrics);
-  }
-}
-
-void IDEReporter::reportSurvivedMutants(const Result &result, const Metrics &metrics) {
-  if (result.getMutationPoints().empty()) {
-    Logger::info() << "No mutants found. Mutation score: infinitely high\n";
-    return;
-  }
-
-  std::set<MutationPoint *> survivedMutants;
-  for (auto &mutationResult : result.getMutationResults()) {
-    auto mutant = mutationResult->getMutationPoint();
-    auto &executionResult = mutationResult->getExecutionResult();
-
-    if (mutantSurvived(executionResult.status)) {
-      survivedMutants.insert(mutant);
-    }
-  }
-
-  Logger::info() << "\nSurvived mutants (" << survivedMutants.size() << "/"
-                 << result.getMutationPoints().size() << "):\n\n";
-
-  SourceManager sourceManager;
-
-  /// It is important that below we iterate over result.getMutationPoints()
-  /// because we want the output to be stable across multiple executions of Mull.
-  /// Optimizing this here was a bad idea: https://github.com/mull-project/mull/pull/640
-  for (auto &mutant : result.getMutationPoints()) {
-    if (survivedMutants.find(mutant) != survivedMutants.end()) {
-      printMutant(sourceManager, *mutant, true);
-    }
-  }
-  if (!survivedMutants.empty()) {
-    Logger::info() << "\n";
-  }
-
-  size_t killedMutantsCount = result.getMutationPoints().size() - survivedMutants.size();
-  auto rawScore = double(killedMutantsCount) / double(result.getMutationPoints().size());
-  auto score = uint(rawScore * 100);
-  Logger::info() << "Mutation score: " << score << "%\n";
-}
-
-void IDEReporter::reportAllMutants(const Result &result, const Metrics &metrics) {
   if (result.getMutationPoints().empty()) {
     Logger::info() << "No mutants found. Mutation score: infinitely high\n";
     return;
   }
 
   std::set<MutationPoint *> killedMutants;
-  std::set<MutationPoint *> survivedMutants;
   for (auto &mutationResult : result.getMutationResults()) {
     auto mutant = mutationResult->getMutationPoint();
     auto &executionResult = mutationResult->getExecutionResult();
 
     if (!mutantSurvived(executionResult.status)) {
       killedMutants.insert(mutant);
-    } else {
-      survivedMutants.insert(mutant);
     }
   }
 
@@ -112,15 +62,17 @@ void IDEReporter::reportAllMutants(const Result &result, const Metrics &metrics)
 
   SourceManager sourceManager;
 
-  Logger::info() << "\nKilled mutants (" << killedMutantsCount << "/"
-                 << result.getMutationPoints().size() << "):\n\n";
-
   /// It is important that below we iterate over result.getMutationPoints()
   /// because we want the output to be stable across multiple executions of Mull.
   /// Optimizing this here was a bad idea: https://github.com/mull-project/mull/pull/640.
-  for (auto &mutant : result.getMutationPoints()) {
-    if (killedMutants.find(mutant) != killedMutants.end()) {
-      printMutant(sourceManager, *mutant, false);
+  if (showKilled && killedMutantsCount > 0) {
+    Logger::info() << "\nKilled mutants (" << killedMutantsCount << "/"
+                   << result.getMutationPoints().size() << "):\n\n";
+
+    for (auto &mutant : result.getMutationPoints()) {
+      if (killedMutants.find(mutant) != killedMutants.end()) {
+        printMutant(sourceManager, *mutant, false);
+      }
     }
   }
 
@@ -128,11 +80,12 @@ void IDEReporter::reportAllMutants(const Result &result, const Metrics &metrics)
                  << result.getMutationPoints().size() << "):\n\n";
 
   for (auto &mutant : result.getMutationPoints()) {
-    if (survivedMutants.find(mutant) != survivedMutants.end()) {
+    if (killedMutants.find(mutant) == killedMutants.end()) {
       printMutant(sourceManager, *mutant, true);
     }
   }
-  if (!survivedMutants.empty()) {
+
+  if (survivedMutantsCount > 0) {
     Logger::info() << "\n";
   }
 
