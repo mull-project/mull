e306cfaadc82e7c980380df1b27c145886cfe90f
Stanislav Pankevich <s.pankevich@gmail.com>
2019-12-16 12:37:14 +0100

    Integration tests: LLVM 9.0.0 prints version in a buggy way

--- tests-lit/tests/tutorials/hello-world/step-1-version/sample.cpp
+++ tests-lit/tests/tutorials/hello-world/step-1-version/sample.cpp
@@ -1,3 +1,4 @@
+/**
 ; RUN: %MULL_EXEC -version 2>&1 | %FILECHECK_EXEC %s --strict-whitespace --match-full-lines
 ; CHECK:Mull: LLVM-based mutation testing
 ; CHECK:https://github.com/mull-project/mull
@@ -6,3 +7,4 @@
 ; CHECK:{{^Date: .*$}}
 ; CHECK:{{^LLVM: \d+\.\d+.\d+$}}
 ; CHECK-EMPTY:
+*/
