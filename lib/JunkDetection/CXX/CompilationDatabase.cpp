62c73b84192c65f5e2697dbf222ee63b5f93d5ab
Stanislav Pankevich <s.pankevich@gmail.com>
2020-03-15 12:45:37 +0100

    Junk detection: command contains escaped quotes (#679)
    Closes #678

--- lib/JunkDetection/CXX/CompilationDatabase.cpp
+++ lib/JunkDetection/CXX/CompilationDatabase.cpp
@@ -4,9 +4,36 @@
 #include <llvm/ADT/SmallString.h>
 #include <llvm/Support/Path.h>
 
+#include <regex>
 #include <sstream>
 
 using namespace mull;
+
+/// Compilation databases such as one created by CMake can be a subject of the
+/// following:
+///
+///     Parameters use shell quoting and shell escaping of quotes, with ‘"’ and
+///     ‘\’ being the only special characters
+///     https://clang.llvm.org/docs/JSONCompilationDatabase.html#format
+///
+/// The compile DB uses shell escaping, and this needs to be undone because
+/// clang::ASTUnit::LoadFromCommandLine() doesn't execute commands in shell.
+///
+/// See "Problem with compiler defines and use of '\"'" (#678) for details.
+/// https://github.com/mull-project/mull/issues/678
+///
+/// Example:
+/// {
+///   "directory": "/src/builds/amd64-linux-mull",
+///   "command":
+///     "/usr/bin/clang++-9 -DQT_TESTCASE_BUILDDIR=\\\"/src/builds/amd64-linux-mull\\\" ...",
+///   "file": /src/tests/TestingUnitTests/ModuleTestSuite.cpp"
+/// }
+static std::string undoEscapingString(std::string &string) {
+  std::string unescaped(std::regex_replace(string, std::regex(R"(\\\")"), "\""));
+  unescaped = std::regex_replace(unescaped, std::regex(R"(\\\\)"), "\\");
+  return unescaped;
+}
 
 static std::vector<std::string> filterFlags(const std::vector<std::string> &flags,
                                             bool stripCompiler) {
@@ -65,6 +92,10 @@ static std::vector<std::string> flagsFromCommand(const CompileCommand &command,
   /// contains a file name in the "arguments" array. Since the file name
   /// itself is not a compilation flag we filter it out.
   flags.erase(std::remove(flags.begin(), flags.end(), command.file), flags.end());
+
+  for (auto &flag: flags) {
+    flag = undoEscapingString(flag);
+  }
 
   for (auto &extra : extraFlags) {
     flags.push_back(extra);
