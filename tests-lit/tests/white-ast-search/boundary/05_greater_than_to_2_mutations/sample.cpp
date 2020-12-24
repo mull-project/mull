e96f33d17802bb3ecbc5830aa7aa9a2cae5a5311
Alex Denisov <alex@lowlevelbits.org>
2020-12-24 23:07:09 +0100

    Run lit tests in single thread

--- tests-lit/tests/white-ast-search/boundary/05_greater_than_to_2_mutations/sample.cpp
+++ tests-lit/tests/white-ast-search/boundary/05_greater_than_to_2_mutations/sample.cpp
@@ -6,11 +6,13 @@ int main() {
   return ! (greater_than(3, 3) == 0);
 }
 
+// clang-format off
+
 /**
 RUN: cd / && %CLANG_EXEC -fembed-bitcode -g %s -o %s.exe
 RUN: cd %CURRENT_DIR
 RUN: sed -e "s:%PWD:%S:g" %S/compile_commands.json.template > %S/compile_commands.json
-RUN: (unset TERM; %MULL_EXEC -debug -enable-ast -test-framework CustomTest -mutators=cxx_gt_to_ge -mutators=cxx_gt_to_le -reporters=IDE -ide-reporter-show-killed -compdb-path %S/compile_commands.json %s.exe 2>&1; test $? = 0) | %FILECHECK_EXEC %s --dump-input=fail --strict-whitespace --match-full-lines
+RUN: (unset TERM; %MULL_EXEC -workers=1 -debug -enable-ast -test-framework CustomTest -mutators=cxx_gt_to_ge -mutators=cxx_gt_to_le -reporters=IDE -ide-reporter-show-killed -compdb-path %S/compile_commands.json %s.exe 2>&1; test $? = 0) | %FILECHECK_EXEC %s --dump-input=fail --strict-whitespace --match-full-lines
 CHECK-NOT:{{^.*[Ee]rror.*$}}
 CHECK-NOT:{{^.*[Ww]arning.*$}}
 
@@ -18,13 +20,13 @@ CHECK:[info] AST Search: looking for mutations in the source files (threads: 1)
 CHECK:[debug] AST Search: recording mutation "Greater Than to Less Or Equal": {{.*}}sample.cpp:2:12
 CHECK:[debug] AST Search: recording mutation "Greater Than to Greater or Equal": {{.*}}sample.cpp:2:12
 
-CHECK:[info] Applying filter: AST mutation filter (threads: 2)
+CHECK:[info] Applying filter: AST mutation filter (threads: 1)
 
 NOTE: These lines we cannot test because they happen in a random order
 [debug] ASTMutationFilter: whitelisting mutation "Greater Than to Greater or Equal": {{.*}}sample.cpp:2:12
 [debug] ASTMutationFilter: whitelisting mutation "Greater Than to Less Or Equal": {{.*}}sample.cpp:2:12
 
-CHECK:[info] Applying filter: junk (threads: 2)
+CHECK:[info] Applying filter: junk (threads: 1)
 CHECK:[debug] ASTMutationStorage: recording mutation "Greater Than to Greater or Equal": {{.*}}sample.cpp:2:12
 
 CHECK:[info] Killed mutants (2/2):
