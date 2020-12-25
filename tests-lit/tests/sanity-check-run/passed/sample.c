85b5865212507ef9c87ca46e8e8e0fac88e1b82c
Alex Denisov <alex@lowlevelbits.org>
2020-12-25 21:36:49 +0100

    Add tests for the original test run

---
+++ tests-lit/tests/sanity-check-run/passed/sample.c
@@ -0,0 +1,11 @@
+int main() {
+  return 0;
+}
+
+// clang-format off
+
+// RUN: cd / && %clang_cc -fembed-bitcode %s -o %s.exe
+// RUN: cd %CURRENT_DIR
+// RUN: (unset TERM; %MULL_EXEC -workers=1 -test-framework CustomTest %s.exe 2>&1; test $? = 0) | %FILECHECK_EXEC %s --dump-input=fail --strict-whitespace --match-full-lines
+// CHECK:[info] No mutants found. Mutation score: infinitely high
+
---
+++ tests-lit/tests/sanity-check-run/timedout/sample.c
@@ -0,0 +1,16 @@
+extern unsigned int sleep(unsigned int);
+int main() {
+  sleep(1);
+  return 1;
+}
+
+// clang-format off
+
+// RUN: cd / && %clang_cc -fembed-bitcode %s -o %s.exe
+// RUN: cd %CURRENT_DIR
+// RUN: (unset TERM; %MULL_EXEC -timeout=100 -workers=1 -test-framework CustomTest %s.exe 2>&1; test $? = 0) | %FILECHECK_EXEC %s --dump-input=fail --strict-whitespace --match-full-lines
+// CHECK:[warning] Original test failed
+// CHECK-NEXT:test: main
+// CHECK-NEXT:status: Timedout
+// CHECK-NEXT:stdout: ''
+// CHECK-NEXT:stderr: ''
