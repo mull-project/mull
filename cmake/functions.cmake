ad596c79d7e4149ea3d129f4ca3ef0067d6265b0
Alex Denisov <alex@lowlevelbits.org>
2021-06-13 13:05:05 +0200

    Decrease the binary size

--- cmake/functions.cmake
+++ cmake/functions.cmake
@@ -15,7 +15,6 @@ macro(add_mull_executable)
   target_link_libraries(${local_NAME} ${local_LINK_WITH} MullVersion)
 
   set_target_properties(${local_NAME} PROPERTIES
-    LINK_FLAGS ${MULL_LINK_FLAGS}
     COMPILE_FLAGS ${MULL_CXX_FLAGS}
   )
   target_include_directories(${local_NAME} PUBLIC
@@ -29,32 +28,3 @@ macro(add_mull_executable)
     RUNTIME DESTINATION bin
   )
 endmacro()
-
-macro(add_mull_internal_executable)
-  set (prefix local)
-  set (optionArguments )
-  set (singleValueArguments NAME)
-  set (multipleValueArguments SOURCES LINK_WITH)
-
-  cmake_parse_arguments(${prefix}
-    "${optionArguments}"
-    "${singleValueArguments}"
-    "${multipleValueArguments}"
-    ${ARGN}
-  )
-
-  add_executable(${local_NAME} ${local_SOURCES})
-  target_link_libraries(${local_NAME} ${local_LINK_WITH})
-
-  set_target_properties(${local_NAME} PROPERTIES
-    LINK_FLAGS ${MULL_LINK_FLAGS}
-    COMPILE_FLAGS ${MULL_CXX_FLAGS}
-  )
-  target_include_directories(${local_NAME} PUBLIC
-    ${MULL_INCLUDE_DIRS}
-  )
-  target_include_directories(${local_NAME} SYSTEM PRIVATE
-    ${THIRD_PARTY_INCLUDE_DIRS}
-  )
-endmacro()
-
