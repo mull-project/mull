d7256dc8e5a4669e1caa1e42eb38a51621a50321
Matthias Bilger <matthias@bilger.info>
2021-11-19 20:40:52 +0100

    Lit tests for version and cmd line info

--- tests-lit/tests/mutation_testing_elements/01_mutation_testing_elements_report/sample.cpp
+++ tests-lit/tests/mutation_testing_elements/01_mutation_testing_elements_report/sample.cpp
@@ -20,7 +20,7 @@ RUN: sed -e "s:%PWD:%S:g" %S/compile_commands.json.template > %S/compile_command
 RUN: (unset TERM; %mull_cxx -linker=%clang_cxx -linker-flags="%sysroot" -debug -mutators=cxx_remove_void_call -reporters=IDE -reporters=Elements --report-dir=%S/Output --report-name=sample -ide-reporter-show-killed -compdb-path %S/compile_commands.json %s.exe 2>&1; test $? = 0) | %filecheck %s --dump-input=fail --strict-whitespace --match-full-lines
 RUN: [[ -f %S/Output/sample.json ]]
 RUN: cat %S/Output/sample.json | filecheck %s --check-prefix=CHECK-JSON
-CHECK-JSON: "mutants": [{"id": "cxx_remove_void_call", "location": {"end": {"column": 26, "line": 6}, "start": {"column": 3, "line": 6}}, "mutatorName": "Removed the call to the function", "replacement": "", "status": "Killed"}]
+CHECK-JSON: "config": {"Build Date": "{{.*}}", "CommandLine": "{{.*}}", {{.*}}, "URL": "https://github.com/mull-project/mull"}{{.*}} "mutants": [{"id": "cxx_remove_void_call", "location": {"end": {"column": 26, "line": 6}, "start": {"column": 3, "line": 6}}, "mutatorName": "Removed the call to the function", "replacement": "", "status": "Killed"}]{{.*}} "framework": {"brandingInformation": {"homepageUrl": "https://github.com/mull-project/mull"}, "name": "Mull", "version": "{{.*}}, LLVM {{.*}}"}
 
 CHECK:[info] Killed mutants (1/1):
 CHECK:{{^.*}}sample.cpp:6:3: warning: Killed: Removed the call to the function [cxx_remove_void_call]
