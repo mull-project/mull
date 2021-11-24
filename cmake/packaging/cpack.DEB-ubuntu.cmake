94184f3e5a2b01dc709a128fe81e19527069bb8a
Alex Denisov <alex@lowlevelbits.org>
2021-11-24 22:46:32 +0100

    Build against official packages on Ubuntu

--- cmake/packaging/cpack.DEB-ubuntu.cmake
+++ cmake/packaging/cpack.DEB-ubuntu.cmake
@@ -3,4 +3,12 @@ set (CPACK_GENERATOR DEB)
 set (CPACK_DEBIAN_PACKAGE_MAINTAINER ${CPACK_PACKAGE_CONTACT})
 set (CPACK_DEBIAN_PACKAGE_DESCRIPTION ${PROJECT_DESCRIPTION})
 
-set (CPACK_DEBIAN_PACKAGE_DEPENDS "zlib1g, libncurses5, libstdc++6, libxml2, libsqlite3-0")
+
+if (NOT NO_PRECOMPILED_LLVM)
+  set (deps "zlib1g, libncurses5, libstdc++6, libxml2, libsqlite3-0")
+else()
+  set (deps "libllvm${LLVM_VERSION_MAJOR}, libxml2, libsqlite3-0")
+endif()
+
+set (CPACK_DEBIAN_PACKAGE_DEPENDS ${deps})
+
