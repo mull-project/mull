08fa1e6345e64d010e49a917a8ca4cd0cccb0ed4
Alex Denisov <alex@lowlevelbits.org>
2022-01-05 22:54:56 +0100

    Force builds against libLLVM.dylib and drop LLVM 7

--- cmake/packaging/cpack.DEB-ubuntu.cmake
+++ cmake/packaging/cpack.DEB-ubuntu.cmake
@@ -3,7 +3,6 @@ set (CPACK_GENERATOR DEB)
 set (CPACK_DEBIAN_PACKAGE_MAINTAINER ${CPACK_PACKAGE_CONTACT})
 set (CPACK_DEBIAN_PACKAGE_DESCRIPTION ${PROJECT_DESCRIPTION})
 
-
 if (NOT NO_PRECOMPILED_LLVM)
   set (deps "zlib1g, libncurses5, libstdc++6, libxml2, libsqlite3-0")
 else()
@@ -11,4 +10,3 @@ else()
 endif()
 
 set (CPACK_DEBIAN_PACKAGE_DEPENDS ${deps})
-
