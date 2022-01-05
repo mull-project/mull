d43fd050f5cff2c3ea2ea62132acd23c45cf3b4a
Alex Denisov <alex@lowlevelbits.org>
2022-01-05 23:54:19 +0100

    Cleanup CMake/Ansible

--- cmake/packaging/cpack.DEB-ubuntu.cmake
+++ cmake/packaging/cpack.DEB-ubuntu.cmake
@@ -3,10 +3,4 @@ set (CPACK_GENERATOR DEB)
 set (CPACK_DEBIAN_PACKAGE_MAINTAINER ${CPACK_PACKAGE_CONTACT})
 set (CPACK_DEBIAN_PACKAGE_DESCRIPTION ${PROJECT_DESCRIPTION})
 
-if (NOT NO_PRECOMPILED_LLVM)
-  set (deps "zlib1g, libncurses5, libstdc++6, libxml2, libsqlite3-0")
-else()
-  set (deps "libllvm${LLVM_VERSION_MAJOR}, libxml2, libsqlite3-0")
-endif()
-
-set (CPACK_DEBIAN_PACKAGE_DEPENDS ${deps})
+set (CPACK_DEBIAN_PACKAGE_DEPENDS "libllvm${LLVM_VERSION_MAJOR}, libxml2, libsqlite3-0")
