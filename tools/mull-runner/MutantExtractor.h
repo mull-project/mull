2763649c8e1b52ee4c98be438a1176b9c55dfb8c
Alex Denisov <alex@lowlevelbits.org>
2022-01-11 20:41:07 +0100

    Deduplicate mutants as part of the mull-runner

--- tools/mull-runner/MutantExtractor.h
+++ tools/mull-runner/MutantExtractor.h
@@ -10,11 +10,11 @@ namespace mull {
 class MutantExtractor {
 public:
   explicit MutantExtractor(Diagnostics &diagnostics);
-  std::vector<std::unique_ptr<Mutant>> extractMutants(const std::string &executable);
   std::vector<std::unique_ptr<Mutant>>
   extractMutants(const std::vector<std::string> &mutantHolders);
 
 private:
+  std::vector<std::string> extractMutants(const std::string &executable);
   Diagnostics &diagnostics;
 };
 
