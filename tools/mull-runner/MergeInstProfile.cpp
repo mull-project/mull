459982ef207e4edd9655eb1b10276811ecd1b9c4
Vilhelm Engström <vilhelm.engstrom@tuta.io>
2022-11-19 13:09:58 +0100

    Avoid warnings caused by ignoring [[nodiscard]] return type
    Since LLVM 14, llvm::InstrProfWriter::write returns an llvm::Error
    rather than void. Propagate potential errors back to the caller
    to avoid triggering warnings

--- tools/mull-runner/MergeInstProfile.cpp
+++ tools/mull-runner/MergeInstProfile.cpp
@@ -1,5 +1,6 @@
 #include "MergeInstProfile.h"
 #include "mull/Diagnostics/Diagnostics.h"
+#include <llvm/Config/llvm-config.h>
 #include <llvm/ProfileData/InstrProfReader.h>
 #include <llvm/ProfileData/InstrProfWriter.h>
 #include <llvm/Support/raw_ostream.h>
@@ -25,6 +26,14 @@ bool mull::mergeRawInstProfile(Diagnostics &diagnostics, const std::string &inpu
     diagnostics.warning("cannot save indexed profile data: "s + ec.message());
     return false;
   }
+
+  // Since LLVM 14, llvm::InstrProfWriter::write returns a [[nodiscard]]-attributed
+  // llvm::Error. Propagate potential errors back to the caller to avoid trigger
+  // unused warnings
+#if LLVM_VERSION_MAJOR >= 14
+  return bool(writer.write(out));
+#else
   writer.write(out);
   return true;
+#endif
 }
