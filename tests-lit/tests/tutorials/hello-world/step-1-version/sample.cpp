05244ed3a3a05de32b68864f927591ae455b621f
Vilhelm Engström <vilhelm.engstrom@tuta.io>
2023-01-15 12:21:45 +0100

    Set mull_version for provisioning of vagrant boxes
    The release CI stage of the provisioning fails due to mull_version not
    being passed as a variable to ansible. Check the environment and, if no
    version is set, fall back on the latest git tag with potential -dirty
    suffix

--- tests-lit/tests/tutorials/hello-world/step-1-version/sample.cpp
+++ tests-lit/tests/tutorials/hello-world/step-1-version/sample.cpp
@@ -4,7 +4,7 @@ RUN: %mull_runner -version 2>&1 | %filecheck %s --dump-input=fail --strict-white
 CHECK:Mull: Practical mutation testing for C and C++
 CHECK:Home: https://github.com/mull-project/mull
 CHECK:Docs: https://mull.readthedocs.io
-CHECK:{{^Version: \d+\.\d+.\d+(-pr[0-9]+|-trunk[0-9]+)?$}}
+CHECK:{{^Version: \d+\.\d+.\d+(-pr[0-9]+|-trunk[0-9]+|-\d+-g[a-zA-Z0-9]+(-dirty)?)?$}}
 CHECK:{{^Commit: [a-h0-9]+$}}
 CHECK:{{^Date: .*$}}
 CHECK:{{^LLVM: \d+\.\d+.\d+$}}
