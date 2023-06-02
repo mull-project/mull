7b84a8009818a28b31e86f5f0116cc16c938b219
AlexDenisov <alex@lowlevelbits.org>
2023-06-02 20:36:47 +0200

    Use realpath for mutant location

--- lib/Path.cpp
+++ lib/Path.cpp
@@ -1,10 +1,16 @@
 #include "mull/Path.h"
 
 #include <llvm/Support/Path.h>
+#include <llvm/Support/FileSystem.h>
 
 std::string mull::absoluteFilePath(const std::string &directory, const std::string &filePath) {
+  auto path = filePath;
   if (!filePath.empty() && !llvm::sys::path::is_absolute(filePath)) {
-    return directory + llvm::sys::path::get_separator().str() + filePath;
+    path = directory + llvm::sys::path::get_separator().str() + filePath;
   }
-  return filePath;
+  llvm::SmallString<PATH_MAX> realPath;
+  if (!llvm::sys::fs::real_path(path, realPath, true)) {
+    return realPath.str().str();
+  }
+  return path;
 }
