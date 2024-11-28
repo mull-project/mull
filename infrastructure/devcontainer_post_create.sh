af61d2c551e26a08376b5697314d4e32b7900a96
Alex Denisov <alex@lowlevelbits.org>
2024-11-28 22:26:30 +0000

    stuff

--- infrastructure/devcontainer_post_create.sh
+++ infrastructure/devcontainer_post_create.sh
@@ -4,12 +4,3 @@ set -e
 set -x
 
 git submodule update --init --recursive
-
-mkdir -p build.debug.dir
-cd build.debug.dir
-cmake -G Ninja -DCMAKE_PREFIX_PATH="/usr/lib/llvm-$1/cmake/;/usr/lib/cmake/clang-$1/" \
-  -DCMAKE_BUILD_TYPE=Debug \
-  -DCMAKE_C_COMPILER=clang-$1 \
-  -DCMAKE_CXX_COMPILER=clang++-$1 \
-  -DCMAKE_CXX_FLAGS=-D_GLIBCXX_USE_CXX11_ABI=1 \
-  ..
