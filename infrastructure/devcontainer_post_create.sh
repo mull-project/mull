2313ffc158c21ca50773d834bc9e435edf5c4630
Alex Denisov <alex@lowlevelbits.org>
2024-12-07 00:34:44 +0100

    Add devcontainer generator

--- infrastructure/devcontainer_post_create.sh
+++ infrastructure/devcontainer_post_create.sh
@@ -5,4 +5,6 @@ set -x
 
 git submodule update --init --recursive
 
+pip3 install -r requirements.txt
+
 ./infrastructure/generator.py cmake --os ubuntu --llvm_version $1
--- infrastructure/generator.py
+++ infrastructure/generator.py
@@ -2,7 +2,7 @@
 
 import argparse
 import pystache
-import pystache.renderer
+import os
 
 
 def cmake(args):
@@ -12,6 +12,27 @@ def cmake(args):
         result = pystache.render(t.read(), template_args)
         with open('CMakePresets.json', "w") as f:
             f.write(result)
+
+
+def devcontainers(os_name, os_version, llvm_version):
+    template_folder = f"infrastructure/templates/devcontainers/{os_name}"
+    container_folder = f".devcontainer/{os_name}_{os_version}_{llvm_version}"
+    os.makedirs(container_folder, exist_ok=True)
+
+    template_args = {"LLVM_VERSION": llvm_version,
+                     "OS": os_name, "OS_VERSION": os_version}
+
+    for template in ['devcontainer.json', "Dockerfile"]:
+        template_name = f"{template_folder}/{template}.mustache"
+        result_filename = f"{container_folder}/{template}"
+        with open(template_name, "r") as t:
+            result = pystache.render(t.read(), template_args)
+            with open(result_filename, "w") as f:
+                f.write(result)
+
+
+def devcontainer(args):
+    devcontainers(args.os, args.os_version, args.llvm_version)
 
 
 def main():
@@ -25,9 +46,19 @@ def main():
     parser_cmake.add_argument('--os', choices=('ubuntu',),
                               help='Select OS for which to generate preset', type=str)
     parser_cmake.add_argument("--llvm_version", type=int)
+
+    parser_devcontainer = subparsers.add_parser(
+        'devcontainer', help='Generates devcontainer files')
+    parser_devcontainer.add_argument('--os', choices=('ubuntu',),
+                                     help='Select OS for which to generate preset', type=str)
+    parser_devcontainer.add_argument("--os_version", type=str)
+    parser_devcontainer.add_argument("--llvm_version", type=int)
+
     args = parser.parse_args()
     if args.cmd == "cmake":
         cmake(args)
+    if args.cmd == "devcontainer":
+        devcontainer(args)
 
 
 if __name__ == "__main__":
