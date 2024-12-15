74ff41e9a5ede3af21b45e7ebaed2c63d9b37243
Alex Denisov <alex@lowlevelbits.org>
2024-12-15 17:23:31 +0100

    Install pre-commit into a devcontainer

--- infrastructure/devcontainer_post_create.sh
+++ infrastructure/devcontainer_post_create.sh
@@ -7,6 +7,7 @@ git config --global --add safe.directory '*'
 git submodule update --init --recursive
 
 pip3 install -r requirements.txt
+pre-commit install
 
 ./infrastructure/generator.py cmake --os $1 --llvm_version $2
 
