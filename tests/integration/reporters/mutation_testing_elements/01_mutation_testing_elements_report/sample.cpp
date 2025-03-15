3444fd9a61520ee99cf7f6497268245222cba95a
Alex Denisov <alex@lowlevelbits.org>
2025-03-15 22:41:39 +0100

    Fix more tests

--- tests/integration/reporters/mutation_testing_elements/01_mutation_testing_elements_report/sample.cpp
+++ tests/integration/reporters/mutation_testing_elements/01_mutation_testing_elements_report/sample.cpp
@@ -14,7 +14,6 @@ int main() {
 // clang-format off
 
 /**
-RUN: cd %CURRENT_DIR
 RUN: sed -e "s:%PWD:%S:g" %S/compile_commands.json.template > %S/compile_commands.json
 RUN: cd %S && %clang_cxx %sysroot %pass_mull_ir_frontend -g %s -o %s-ir.exe
 RUN: unset TERM; %mull_runner -debug -reporters=IDE -reporters=Elements --report-dir=%S/Output --report-name=sample-ir -ide-reporter-show-killed %s-ir.exe 2>&1 | %filecheck %s --dump-input=fail --strict-whitespace --match-full-lines
