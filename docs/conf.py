3930b12fe414b5fff171676243aa8051a6a79469
Alex Denisov <alex@lowlevelbits.org>
2025-10-19 22:34:31 +0200

    Fix sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -20,16 +20,20 @@ import guzzle_sphinx_theme
 
 # -- Project information -----------------------------------------------------
 
+
 def project_version():
-    with open("../CMakeLists.txt") as f:
+    with open("../MODULE.bazel") as f:
         for line in f:
-            m = re.search('MULL_VERSION (\d+.\d+.\d+)', line)
+            m = re.search(r'module\(name = "mull", version = "(\d+.\d+.\d+)"\)', line)
             if m:
                 return m.group(1)
 
-project = 'Mull'
-copyright = '2016-' + str(datetime.datetime.now().year) + ', Mull Project'
-author = 'Alex Denisov <alex@lowlevelbits.org>, Stanislav Pankevich <s.pankevich@gmail.com>'
+
+project = "Mull"
+copyright = "2016-" + str(datetime.datetime.now().year) + ", Mull Project"
+author = (
+    "Alex Denisov <alex@lowlevelbits.org>, Stanislav Pankevich <s.pankevich@gmail.com>"
+)
 
 version = project_version()
 release = version
@@ -40,16 +44,15 @@ print("Generating docs for Mull " + version)
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -59,16 +62,16 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 #
 html_theme_path = guzzle_sphinx_theme.html_theme_path()
 extensions.append("guzzle_sphinx_theme")
-html_theme = 'guzzle_sphinx_theme'
+html_theme = "guzzle_sphinx_theme"
 
 html_theme_options = {
     "project_nav_name": "Mull",
 }
-html_sidebars = { '**': ['globaltoc.html', 'searchbox.html'] }
+html_sidebars = {"**": ["globaltoc.html", "searchbox.html"]}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
-html_css_files = [ 'custom.css' ]
-master_doc = 'index'
+html_static_path = ["_static"]
+html_css_files = ["custom.css"]
+master_doc = "index"
