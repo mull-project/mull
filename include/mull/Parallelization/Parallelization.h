8b7ed95208e5eca0d7a629c7200a4e0486f8e714
Alex Denisov <alex@lowlevelbits.org>
2026-03-08 18:25:43 +0100

    remove more

--- include/mull/Parallelization/Parallelization.h
+++ include/mull/Parallelization/Parallelization.h
@@ -4,10 +4,8 @@
 #include "mull/Parallelization/TaskExecutor.h"
 
 #include "mull/Parallelization/Tasks/ApplyMutationTask.h"
-#include "mull/Parallelization/Tasks/DryRunMutantExecutionTask.h"
 #include "mull/Parallelization/Tasks/FunctionFilterTask.h"
 #include "mull/Parallelization/Tasks/InstructionSelectionTask.h"
-#include "mull/Parallelization/Tasks/MutantExecutionTask.h"
 #include "mull/Parallelization/Tasks/MutantPreparationTasks.h"
 #include "mull/Parallelization/Tasks/MutationFilterTask.h"
 #include "mull/Parallelization/Tasks/SearchMutationPointsTask.h"
