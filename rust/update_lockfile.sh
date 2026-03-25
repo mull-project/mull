bb31478a8a60d1e178782400364c34ce36a38f7d
Alex Denisov <alex@lowlevelbits.org>
2026-03-25 13:17:24 +0100

    Add shas for rust binaries

--- rust/update_lockfile.sh
+++ rust/update_lockfile.sh
@@ -3,3 +3,6 @@ set -euo pipefail
 
 CARGO="$1"
 "$CARGO" update --manifest-path "$BUILD_WORKSPACE_DIRECTORY/rust/Cargo.toml" "${@:2}"
+
+echo "Cargo.lock updated. Run the following to repin the Bazel crate universe:"
+echo "  CARGO_BAZEL_REPIN=1 bazel build //..."
